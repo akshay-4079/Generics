9228e0ae21163700c57860c396d418a6aa7cfebb
Akshay <akshaysrikumar@gmail.com>
2019-02-26 08:15:23 -0600

    c#

---
+++ GenericsEasy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericsEasy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MyGenericClass<string> strGenericClass = new MyGenericClass<string>("Hello World");
+            MyGenericClass<int> intGenericClass = new MyGenericClass<int>(123456);
+            MyGenericClass<double> doubleGenericClass = new MyGenericClass<double>(1234.56);
+            int result1 = intGenericClass.genericMethod(1325);
+            string result = strGenericClass.genericMethod("Akshay");
+            double result2 = doubleGenericClass.genericMethod(1325.1234);
+        }
+    }
+}
---
+++ GenericsHard/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace GenericsHard
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Restricted Generics");
+            MyGenericClass<string> strGenericClass1 = new MyGenericClass<string>("Hello World");
+
+            strGenericClass1.genericMethod("Hello World!");
+
+            MyGenericClass<Student> strGenericClass2 = new MyGenericClass<Student>(new Student());
+
+            MyGenericClass1<Student> strGen = new MyGenericClass1<Student>(new Student());
+            //MyGenericClass<int> intGenericClass = new MyGenericClass<int>(10);
+            strGen.genericMethod1<string>(new Student(), "Hi");
+            strGen.genericMethod1<string>(new Student(), "Hd");
+    //strGen.genericMethod1<string>(new Student(), 123);
+        }
+    }
+
+
+    class MyGenericClass<T> where T : class
+    {
+        private T genericMemberVariable;
+
+        public MyGenericClass(T value)
+        {
+            genericMemberVariable = value;
+        }
+
+        public T genericMethod(T genericParameter)
+        {
+            Console.WriteLine("Parameter type: {0}, value: {1}", typeof(T).ToString(), genericParameter);
+            Console.WriteLine("Return type: {0}, value: {1}", typeof(T).ToString(), genericMemberVariable);
+
+            return genericMemberVariable;
+        }
+
+        public T genericProperty { get; set; }
+    }
+    class MyGenericClass1<T> where T : class
+    {
+        private T genericMemberVariable;
+
+        public MyGenericClass1(T value)
+        {
+            genericMemberVariable = value;
+        }
+        public T genericMethod1<U>(T genericParameter, U anotherGenericType) where U : class
+        {
+               Console.WriteLine("In the Function");
+                return genericParameter;
+
+        }
+
+
+
+
+    }
+
+    public class Student
+    {
+    }
+
+}
